0be13d0ef9dbe7523f7aef89f144fcbe53ae9285
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-05 15:25:33 -0300

    first commit

---
+++ src/pages/cadastro/index.tsx
@@ -0,0 +1,7 @@
+export default function Cadastro() {
+    return (
+        <div>
+            <h1>Page Cadastro</h1>
+        </div>
+    )
+}
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <>
+            <h1>Page Login</h1>
+        </>
+    )
+
+}
